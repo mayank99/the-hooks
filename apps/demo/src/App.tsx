32938c7dc44c4e0611ad420c4f24014a5d8e28c1
Mayank <mayank99@users.noreply.github.com>
2022-06-09 17:03:46 -0400

    Improve demo

--- apps/demo/src/App.tsx
+++ apps/demo/src/App.tsx
@@ -1,10 +1,23 @@
 import * as React from 'react';
+import { useCSSTransition } from 'use-css-transition';
+import './App.css';
 
 export const App = () => {
+  const duration = 500;
+  const [isVisible, toggle] = React.useReducer((s) => !s, false);
+  const [state, ref] = useCSSTransition({ isVisible, duration });
+
   return (
-    <>
-      <div>Hello world</div>
-    </>
+    <main>
+      <button onClick={toggle}>Show</button>
+      {state != null && (
+        <div
+          ref={ref}
+          style={{ '--duration': `${duration}ms` }}
+          data-state={state}
+        />
+      )}
+    </main>
   );
 };
 
