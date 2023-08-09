47230d0b65717aaaf3727dc50f4a2d1b09479a27
Mayank <9084735+mayank99@users.noreply.github.com>
2023-08-09 00:47:05 +0000

    the-hooks

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 A collection of smol hooks that are useful in everyday react.
 
-- [@smol-hooks/use-animated-toggle](./packages/use-animated-toggle) [WIP]
-- [@smol-hooks/use-safe-context](./packages/use-safe-context) [WIP]
+- [@the-hooks/use-animated-toggle](./packages/use-animated-toggle) [WIP]
+- [@the-hooks/use-safe-context](./packages/use-safe-context) [WIP]
--- apps/demo/src/App.tsx
+++ apps/demo/src/App.tsx
@@ -1,27 +1,27 @@
 import * as React from 'react';
-import { useAnimatedToggle } from '@smol-hooks/use-animated-toggle';
+import { useAnimatedToggle } from '@the-hooks/use-animated-toggle';
 import './App.css';
 
 export const App = () => {
-  const ref = React.useRef<HTMLDivElement>(null);
-  const [isVisible, toggle] = useAnimatedToggle(ref, {
-    duration: 500,
-    enterKeyframes: [
-      { opacity: 0, transform: 'translateX(-100%)' },
-      { opacity: 1, transform: 'translateX(0)' },
-    ],
-    exitKeyframes: { opacity: 0, transform: 'translateX(100%)' },
-  });
+	const ref = React.useRef<HTMLDivElement>(null);
+	const [isVisible, toggle] = useAnimatedToggle(ref, {
+		duration: 500,
+		enterKeyframes: [
+			{ opacity: 0, transform: 'translateX(-100%)' },
+			{ opacity: 1, transform: 'translateX(0)' },
+		],
+		exitKeyframes: { opacity: 0, transform: 'translateX(100%)' },
+	});
 
-  return (
-    <main>
-      <h1>
-        <code>useAnimatedToggle</code> demo
-      </h1>
-      <button onClick={() => toggle()}>{!isVisible ? 'Show' : 'Hide'}</button>
-      {isVisible && <div ref={ref} />}
-    </main>
-  );
+	return (
+		<main>
+			<h1>
+				<code>useAnimatedToggle</code> demo
+			</h1>
+			<button onClick={() => toggle()}>{!isVisible ? 'Show' : 'Hide'}</button>
+			{isVisible && <div ref={ref} />}
+		</main>
+	);
 };
 
 export default App;
